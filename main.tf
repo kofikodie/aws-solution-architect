b761d7bce2d4a1adb9303200f3936b3f07d28e9e
Kofi Kodie <k.kodieaddo@gmail.com>
2024-02-02 12:33:59 +0100

    Add .gitignore, .terraform.lock.hcl, main.tf, provider.tf, and variables.tf files

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  cloud {
+    organization = "dragon-ws"
+
+    workspaces {
+      name = "saa-c03"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,13 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+}
+
+provider "aws" {
+  region     = "eu-west-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
