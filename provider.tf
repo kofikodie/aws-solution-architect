ed9a9fd2d920efa626fa10da9bcdd8c90403d022
Kofi Kodie <k.kodieaddo@gmail.com>
2024-02-08 17:33:39 +0100

    Refactor AWS provider configuration and add access key and secret key variables

--- provider.tf
+++ provider.tf
@@ -1,19 +1,5 @@
-variable "aws_access_key" {
-  description = "AWS access key"
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key"
-}
-
-variable "aws_region" {
-  description = "AWS region"
-}
-
 provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
-
-data "aws_availability_zones" "available" {}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+}
+
 variable "aws_region" {
   default = "eu-west-1"
 }
+
+data "aws_availability_zones" "available" {}
