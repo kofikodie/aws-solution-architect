910013effa9fdc2dad69b9583c3fd9784f649891
Kofi Kodie <k.kodieaddo@gmail.com>
2024-02-15 02:30:24 +0100

    Add secondary AWS provider configuration
    Add aws_secondary_region variable

--- provider.tf
+++ provider.tf
@@ -3,3 +3,10 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
+
+provider "aws" {
+  alias      = "central"
+  region     = var.aws_secondary_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
--- variables.tf
+++ variables.tf
@@ -10,4 +10,8 @@ variable "aws_region" {
   default = "eu-west-1"
 }
 
+variable "aws_secondary_region" {
+  default = "eu-central-1"
+}
+
 data "aws_availability_zones" "available" {}
