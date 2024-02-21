60e504ec2813b49485bae1e0e4548258ac5db607
Kofi Kodie <k.kodieaddo@gmail.com>
2024-02-21 18:20:40 +0100

    Refactor infrastructure modules and add SQS module

--- main.tf
+++ main.tf
@@ -15,67 +15,28 @@ terraform {
   }
 }
 
-
 provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
-provider "aws" {
-  alias      = "secondary"
-  region     = var.aws_secondary_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
-
-module "vpc_west" {
+module "vpn" {
   source = "./modules/vpc"
-  name   = "vpc-west"
-  azs    = ["eu-west-1a", "eu-west-1b"]
-  providers = {
-    aws = aws
-  }
+
+  name = "saa-c03-vpn"
+  azs  = ["eu-west-1a", "eu-west-1b"]
 }
 
-module "vpc_central" {
-  source = "./modules/vpc"
-  name   = "vpc-central"
-  azs    = ["eu-central-1a", "eu-central-1b"]
-  providers = {
-    aws = aws.secondary
-  }
+module "sqs" {
+  source = "./modules/sqs"
+
+  sqs_queue_name = "saa-c03-sqs"
 }
 
-module "ec2_instance_west" {
-  source = "./modules/ec2_instance"
+module "sqs_fifo" {
+  source = "./modules/sqs"
 
-  instance_name = "west-instance"
-  ami           = "ami-0766b4b472db7e3b9"
-  instance_type = "t2.micro"
-  subnet_id     = module.vpc_west.subnet_id[0]
-  vpc_id        = module.vpc_west.vpc_id
-  providers = {
-    aws = aws
-  }
-}
-
-module "ec2_instance_central" {
-  source = "./modules/ec2_instance"
-
-  instance_name = "central-instance"
-  ami           = "ami-03cceb19496c25679"
-  instance_type = "t2.micro"
-  subnet_id     = module.vpc_central.subnet_id[0]
-  vpc_id        = module.vpc_central.vpc_id
-  providers = {
-    aws = aws.secondary
-  }
-}
-
-module "global_accelerator" {
-  source                           = "./modules/global_accelerator"
-  name                             = "global-accelerator"
-  ec2_instance_west_instance_id    = module.ec2_instance_west.instance_id
-  ec2_instance_central_instance_id = module.ec2_instance_central.instance_id
+  sqs_queue_name = "saaC03Sqs.fifo"
+  fifo_queue     = true
 }
---
+++ modules/sqs/output.tf
@@ -0,0 +1,3 @@
+output "queue_url" {
+  value = aws_sqs_queue.saa_c03_sqs.id
+}
