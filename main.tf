5478fe8c63c102a2e177762b292106734541a72c
Kofi Kodie <k.kodieaddo@gmail.com>
2024-03-06 15:54:12 +0100

    add App Runner module

--- main.tf
+++ main.tf
@@ -20,3 +20,110 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
+
+locals {
+  name     = "app-runner"
+  azs      = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  vpc_cidr = "10.0.0.0/16"
+  tags = {
+    Example    = local.name
+    GithubRepo = "terraform-aws-app-runner"
+    GithubOrg  = "terraform-aws-modules"
+  }
+}
+
+
+module "app_runner_image_base" {
+  source = "terraform-aws-modules/app-runner/aws"
+
+  service_name = "${local.name}-image-base"
+
+  auto_scaling_configurations = {
+    mini = {
+      name            = "mini"
+      max_concurrency = 20
+      max_size        = 5
+      min_size        = 1
+
+      tags = {
+        Type = "Mini"
+      }
+    }
+
+    mega = {
+      name            = "mega"
+      max_concurrency = 200
+      max_size        = 25
+      min_size        = 5
+
+      tags = {
+        Type = "MEGA"
+      }
+    }
+  }
+
+  source_configuration = {
+    auto_deployments_enabled = false
+    image_repository = {
+      image_configuration = {
+        port = 80
+      }
+      image_identifier      = "public.ecr.aws/docker/library/httpd:latest"
+      image_repository_type = "ECR_PUBLIC"
+    }
+  }
+
+  create_vpc_connector          = true
+  vpc_connector_subnets         = module.vpc.private_subnets
+  vpc_connector_security_groups = [module.security_group.security_group_id]
+  network_configuration = {
+    egress_configuration = {
+      egress_type = "VPC"
+    }
+  }
+
+  enable_observability_configuration = true
+
+  health_check_configuration = {
+    protocol = "HTTP"
+    path     = "/"
+    port     = 80
+  }
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
+
+  enable_nat_gateway      = false
+  single_nat_gateway      = true
+  enable_dns_hostnames    = true
+  map_public_ip_on_launch = false
+
+  tags = local.tags
+}
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 4.0"
+
+  name        = local.name
+  description = "Security group for AppRunner connector"
+  vpc_id      = module.vpc.vpc_id
+
+  egress_rules       = ["http-80-tcp"]
+  egress_cidr_blocks = module.vpc.private_subnets_cidr_blocks
+
+  tags = local.tags
+}
+
+output "image_base_service_url" {
+  description = "A subdomain URL that App Runner generated for this service. You can use this URL to access your service web application"
+  value       = module.app_runner_image_base.service_url
+}
