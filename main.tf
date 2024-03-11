8d7117545b9e3ea12b87a344fdde19d1f735ef9a
Kofi Kodie <k.kodieaddo@gmail.com>
2024-03-11 14:58:31 +0100

    Add S3 access logs SQL script and create Athena database

--- main.tf
+++ main.tf
@@ -20,3 +20,13 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
+
+
+resource "aws_s3_bucket" "main_bucket" {
+  bucket = "saa-c03-bucket"
+}
+
+resource "aws_athena_database" "main_athena_database" {
+  name   = "saa_c03_database"
+  bucket = aws_s3_bucket.main_bucket.id
+}
