c771b994eea55451e2cba55d9123c5e974787bf0
Kofi Kodie <k.kodieaddo@gmail.com>
2024-03-12 16:58:01 +0100

    Add eventbridge module to main.tf

--- main.tf
+++ main.tf
@@ -20,3 +20,36 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
+
+module "eventbridge" {
+  source = "terraform-aws-modules/eventbridge/aws"
+
+  bus_name        = "central-event-bus-v2"
+  create_archives = true
+  archives = {
+    "central-event-bus-archive" = {
+      description    = "Central event bus archive"
+      retention_days = 7
+    }
+  }
+
+  rules = {
+    central_event_bus_rule = {
+      description = "Central event bus rule"
+      event_pattern = jsonencode({
+        source = ["aws.ec2"]
+      })
+      enabled = true
+    }
+  }
+
+  targets = {
+    central_event_bus_target = [
+      {
+        arn = aws_sns_topic.central_event_bus_sns.arn
+        id  = "central_event_bus_sns"
+      }
+    ]
+  }
+  create_schemas_discoverer = true
+}
