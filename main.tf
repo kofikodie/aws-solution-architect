6164c56f1f0e46ec77a441ec6de10f15ef5ad3d2
Kofi Kodie <k.kodieaddo@gmail.com>
2024-03-18 13:19:30 +0100

    Add VPC, subnets, and security group resources

--- main.tf
+++ main.tf
@@ -20,3 +20,80 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id                  = aws_vpc.main.id
+  availability_zone       = "eu-west-1a"
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public_subnet_a"
+  }
+}
+
+resource "aws_subnet" "public_subnet_b" {
+  vpc_id                  = aws_vpc.main.id
+  availability_zone       = "eu-west-1b"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public_subnet_b"
+  }
+}
+
+resource "aws_subnet" "private_subnet_a" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "eu-west-1a"
+  cidr_block        = "10.0.16.0/20"
+
+  tags = {
+    Name = "private_subnet_a"
+  }
+}
+
+resource "aws_subnet" "private_subnet_b" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "eu-west-1b"
+  cidr_block        = "10.0.32.0/20"
+
+  tags = {
+    Name = "private_subnet_b"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami             = "ami-0c55b159cbfafe1f0"
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.public_subnet_a.id
+  security_groups = [aws_security_group.ssh.id]
+
+  tags = {
+    Name = "web"
+  }
+}
+
+//create a security group for ssh access
+resource "aws_security_group" "ssh" {
+  name        = "ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
