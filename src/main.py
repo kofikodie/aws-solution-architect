ae29ba84d658f602baa4b3385f52a3e40a56b269
Kofi Kodie <k.kodieaddo@gmail.com>
2024-11-18 15:39:34 +0100

    add rand lib + new image

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
-from typing import Union
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int):
+    qty = random.randint(0, 10)
+    return {"item_id": item_id, "qty": qty}
